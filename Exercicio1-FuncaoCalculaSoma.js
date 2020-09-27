4039b66fbb4530a70d47fb999fed70c620f7d6d1
KatiaJTartari <70042571+KatiaJTartari@users.noreply.github.com>
2020-09-27 13:57:19 -0300

    Add files via upload

---
+++ Exercicio1-FuncaoCalculaSoma.js
@@ -0,0 +1,9 @@
+
+function calculo() {
+    var a=parseFloat(document.fr.txtA.value);
+    var b=parseFloat(document.fr.txtB.value);
+    var s=(a+b);
+    document.getElementById("resultado").innerHTML=s;
+}
+
+
